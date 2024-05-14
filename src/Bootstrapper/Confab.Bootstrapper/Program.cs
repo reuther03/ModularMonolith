db0a632756216174112e8817e5ec32e65dd10630
michaldrozd <michaldrozd03@gmail.com>
2024-05-14 23:51:40 +0200

    Initial

---
+++ src/Bootstrapper/Confab.Bootstrapper/Program.cs
@@ -0,0 +1,18 @@
+var builder = WebApplication.CreateBuilder(args);
+
+var services = builder.Services;
+services.AddEndpointsApiExplorer();
+services.AddSwaggerGen();
+
+var app = builder.Build();
+
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.Run();
