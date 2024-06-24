7193cc53a88dc541bfc25b3835a90106a1cf1c67
michaldrozd <michaldrozd03@gmail.com>
2024-06-24 23:58:24 +0200

    Added HostRepository

---
+++ src/Modules/Confab.Modules.Conferences.Core/DAL/Repositories/HostRepository.cs
@@ -0,0 +1,37 @@
+﻿using Confab.Modules.Conferences.Core.Entities;
+using Confab.Modules.Conferences.Core.Repositories;
+using Microsoft.EntityFrameworkCore;
+
+namespace Confab.Modules.Conferences.Core.DAL.Repositories;
+
+internal class HostRepository : IHostRepository
+{
+    private readonly ConferencesDbContext _context;
+
+    public HostRepository(ConferencesDbContext context)
+    {
+        _context = context;
+    }
+
+    public Task<Host> GetAsync(Guid id) => _context.Hosts.Include(x=> x.Conferences).SingleOrDefaultAsync(x => x.Id == id);
+
+    public Task<IReadOnlyList<Host>> BrowseAsync()
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task AddAsync(Host host)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task UpdateAsync(Host host)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task DeleteAsync(Host host)
+    {
+        throw new NotImplementedException();
+    }
+}
--- src/Shared/Confab.Shared.Infrastructure/obj/Debug/net8.0/Confab.Shared.Infrastructure.AssemblyInfo.cs
+++ src/Shared/Confab.Shared.Infrastructure/obj/Debug/net8.0/Confab.Shared.Infrastructure.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Confab.Shared.Infrastructure")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+45735725955301f17ae61aa87dfc682b29450495")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8fddb513c665eea56c6736c762c6a928ce0c0099")]
 [assembly: System.Reflection.AssemblyProductAttribute("Confab.Shared.Infrastructure")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Confab.Shared.Infrastructure")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
